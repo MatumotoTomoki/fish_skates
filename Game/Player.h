0194c49cf9afb327caba66f31b4e98a4af25d3a6
MitumuneJuria <ca01254020@st.kawahara.ac.jp>
2026-03-16 10:09:52 +0900

    タイトルとゲームオーバー追加 (#13)

--- Game/Player.h
+++ Game/Player.h
@@ -30,6 +30,7 @@ public:
 	bool m_superJump = false;
 	float m_hp = -1.05f;
 	float m_o2 = -1.05f;
+	float m_count = 0.0f;
 	int m_i = 0;
 	SpriteRender m_sprite;
 	Pengin* m_pengin;
---
+++ Game/Title.cpp
@@ -0,0 +1,34 @@
+﻿#include "stdafx.h"
+#include "Title.h"
+#include "Game.h"
+
+bool Title::Start() {
+	return true;
+}
+
+void Title::Update() {
+	if (g_pad[0]->IsTrigger(enButtonA)) {
+		m_flug = true;
+		m_font.SetText(L"3");
+	}
+	if (m_flug == true) {
+		m_count -= 0.015f;
+	}
+	if (m_count <= 2.0f) {
+		m_font.SetText(L"2");
+	}
+	if (m_count <= 1.0f) {
+		m_font.SetText(L"1");
+	}
+	if (m_count <= 0.0f) {
+		m_font.SetText(L"GO!");
+	}
+	if (m_count <= -1.0f) {
+		NewGO<Game>(0, "game");
+		DeleteGO(this);
+	}
+}
+
+void Title::Render(RenderContext& rc) {
+	m_font.Draw(rc);
+}
