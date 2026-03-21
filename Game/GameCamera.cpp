b034dc0b09c6e666565c108de85683f4452f3fa1
MitumuneJuria <ca01254020@st.kawahara.ac.jp>
2026-03-21 16:59:34 +0900

    カメラの回転角度の制限 (#24)

--- Game/GameCamera.cpp
+++ Game/GameCamera.cpp
@@ -49,10 +49,16 @@ void GameCamera::Update() {
 		qRot.Apply(m_toCameraPos);
 		Vector3 toPosDir = m_toCameraPos;
 		toPosDir.Normalize();
-		if (toPosDir.y < -0.9f) {
+		if (toPosDir.y < -0.15f) {
 			m_toCameraPos = toCameraPosOld;
 		}
 		else if (toPosDir.y > 0.9f) {
+			m_toCameraPos = toCameraPosOld;
+		}
+		if (toPosDir.x < -0.8f) {
+			m_toCameraPos = toCameraPosOld;
+		}
+		else if (toPosDir.x > 0.8f) {
 			m_toCameraPos = toCameraPosOld;
 		}
 	
