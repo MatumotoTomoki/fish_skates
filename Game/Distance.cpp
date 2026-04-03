a4327e0a1b43c8473192d2251dd9c0de77fa1c16
MitsumuneJuria <ca01254020@st.kawahara.ac.jp>
2026-04-03 11:26:01 +0900

    距離表示が999からになる不具合修正

--- Game/Distance.cpp
+++ Game/Distance.cpp
@@ -5,7 +5,6 @@
 bool Distance::Start() {
 	m_pos = { 0.0f,0.0f,999.0f };
 	m_player = FindGO<Player>("Player");
-	m_model.Init("Assets/modelData/distance.tkm");
 	m_model.SetPosition(m_pos);
 	m_model.Update();
 	return true;
@@ -29,20 +28,18 @@ void Distance::Update() {
 	case 3:
 		m_pos = { 65.0f,0.0f,6500.0f };
 		break;
-	/*case 4:
-		m_pos = { 0.0f,0.0f,5000.0f };
-		break;*/
 	default:
 		DeleteGO(this);
 		break;
 	}
 	Vector3 center = m_pos;
-	wchar_t distanceText[64];
 	Vector3 diff = center - m_player->m_position;
+	diff /= 10.0f;
 	float distance = diff.Length();
 	if (distance >= 999.0f) {
 		distance = 999.0f;
 	}
+	wchar_t distanceText[64];
 	swprintf(distanceText, 64, L"%.0f", distance);
 	m_font.SetText(distanceText);
 	m_font.SetColor(0.0f, 1.0f, 1.0f, 1.0f);
@@ -62,5 +59,4 @@ void Distance::Update() {
 
 void Distance::Render(RenderContext& rc) {
 	m_font.Draw(rc);
-	//m_model.Draw(rc);
 }
