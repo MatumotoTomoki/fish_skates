ac9800fda4c0ee31c0b3e79086b72e4f6ce46cb4
MatumotoTomoki <ca01254019@st.kawahara.ac.jp>
2026-04-21 13:25:23 +0900

    方向を示す矢印を追加

---
+++ Game/Arrow.cpp
@@ -0,0 +1,35 @@
+﻿#include "stdafx.h"
+#include "Arrow.h"
+#include "Player.h"
+#include "Distance.h"
+bool Arrow::Start()
+{
+	m_modelRender.Init("Assets/modelData/Arrow.tkm");
+	m_player = FindGO<Player>("Player");
+	m_distance = FindGO<Distance>("Distance");
+	m_position = { m_player->m_position.x,10.0f,m_player->m_position.z };
+	m_modelRender.SetPosition(m_position);
+	Quaternion rot;
+	rot.SetRotationDegX(90.0f);
+	m_modelRender.SetRotation(rot);
+	m_modelRender.Update();
+	return true;
+}
+
+void Arrow::Update()
+{
+	m_position = { m_player->m_position.x,m_player->m_position.y,m_player->m_position.z };
+	m_position.y += 100.0f;
+	m_modelRender.SetPosition(m_position);
+	Vector3 toArrow = m_position - m_distance->m_pos;
+	toArrow.Normalize();
+	float toAlloedir = atan2f(toArrow.x, toArrow.z);
+	m_rot.SetRotationY(toAlloedir);
+	m_modelRender.SetRotation(m_rot);
+	m_modelRender.Update();
+}
+
+void Arrow::Render(RenderContext& rc)
+{
+	m_modelRender.Draw(rc);
+}
--- Game/Distance.h
+++ Game/Distance.h
@@ -5,10 +5,11 @@ class Distance : public IGameObject
 	bool Start();
 	void Update();
 	void Render(RenderContext& rc);
-	Vector3 m_pos;
 	Player* m_player;
 	FontRender m_font;
 	ModelRender m_model;
 	int m_hollState = 0;
+public:
+	Vector3 m_pos;
 };
 
