98ecadff04bb6e81846152654e010ade83f9cb57
MitumuneJuria <ca01254020@st.kawahara.ac.jp>
2026-04-25 16:01:48 +0900

    カメラの回転の制限を緩くする (#88)

--- Game/GameCamera.cpp
+++ Game/GameCamera.cpp
@@ -41,7 +41,7 @@ void GameCamera::Update() {
     float len = horizontalDir.Length();   // ← これが必要！
     if (len > 0.001f) {
         horizontalDir /= len;
-        if (horizontalDir.x < -0.9f or horizontalDir.x > 0.9f) {
+        if (horizontalDir.x < -0.999f or horizontalDir.x > 0.999f) {
             m_toCameraPos = oldPosYaw;
         }
     }
