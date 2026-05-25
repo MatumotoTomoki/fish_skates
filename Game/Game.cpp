d9a2a74cdb19b1393e83237cb3c6a58b7052f199
MatumotoTomoki <ca01254019@st.kawahara.ac.jp>
2026-05-25 12:23:01 +0900

    水に浸かった時のSE追加

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -31,6 +31,7 @@ void Game::Preload() {
 	g_soundEngine->ResistWaveFileBank(15, "Assets/sound/superjump.wav");
 	g_soundEngine->ResistWaveFileBank(16, "Assets/sound/chase.wav");
 	g_soundEngine->ResistWaveFileBank(17, "Assets/sound/Menu.wav");
+	g_soundEngine->ResistWaveFileBank(18, "Assets/sound/Hial.wav");
 	EffectEngine::GetInstance()->ResistEffect(0, u"Assets/effect/magic_sphere.efk");
 	EffectEngine::GetInstance()->ResistEffect(1, u"Assets/effect/Wave.efk");
 	EffectEngine::GetInstance()->ResistEffect(2, u"Assets/effect/jump.efk");
