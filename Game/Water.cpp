97d7d371dc1d37d3eef1b102bc72157719bc9b58
MitumuneJuria <ca01254020@st.kawahara.ac.jp>
2026-05-25 14:27:18 +0900

    ダメージ音追加 (#123)

--- Game/Water.cpp
+++ Game/Water.cpp
@@ -5,6 +5,7 @@ bool Water::Start() {
 	m_render.Init("Assets/modelData/water.tkm");
 	m_render.SetScale(9000.0f, 10.0f, 9000.0f);
 	m_pos.x = 500000.0f;
+	m_pos.y = -10.0f;
 	m_pos.z = -50000.0f;
 	m_render.SetPosition(m_pos);
 	m_render.Update();
