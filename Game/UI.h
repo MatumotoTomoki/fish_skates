4d153b5c0ccff6d92637182d15ede4c110486c42
tonokouti0519 <Ca01254015tonokouti>
2026-06-17 10:36:42 +0900

    juria

--- Game/UI.h
+++ Game/UI.h
@@ -11,6 +11,8 @@ class UI : public IGameObject
 	SpriteRender m_spriteRender4;
 	SpriteRender m_spriteRender5;
 	SpriteRender m_startRender;
+	SpriteRender m_warningRender;
+	bool m_warningFlag = false;
 	Player* m_player;
 	int m_hpUI = 0;
 	int m_o2UI = 0;
