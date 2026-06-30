9861e9dcc0e887a83630c9c5ce8c454e3fd8c945
MitsumuneJuria <ca01254020@st.kawahara.ac.jp>
2026-06-30 12:18:11 +0900

    こーど修正

--- Game/Arrow.cpp
+++ Game/Arrow.cpp
@@ -7,24 +7,18 @@ bool Arrow::Start() {
 	m_modelRender.SetShadowCasterFlag(false);
 	m_player = FindGO<Player>("Player");
 	m_distance = FindGO<Distance>("Distance");
-	m_position = { m_player->m_position.x,10.0f,m_player->m_position.z };
-	m_modelRender.SetPosition(m_position);
-	Quaternion rot;
-	rot.SetRotationDegX(90.0f);
-	m_modelRender.SetRotation(rot);
 	m_modelRender.SetScale(0.7f, 0.7f, 0.7f);
-	m_modelRender.Update();
 	return true;
 }
 
 void Arrow::Update() {
 	m_position = { m_player->m_position.x,m_player->m_position.y,m_player->m_position.z };
 	m_position.y += 100.0f;
-	m_modelRender.SetPosition(m_position);
 	Vector3 toArrow = m_position - m_distance->m_pos;
 	toArrow.Normalize();
 	float toAlloedir = atan2f(toArrow.x, toArrow.z);
 	m_rot.SetRotationY(toAlloedir);
+	m_modelRender.SetPosition(m_position);
 	m_modelRender.SetRotation(m_rot);
 	m_modelRender.Update();
 }
