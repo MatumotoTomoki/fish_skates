a25f5837194bd9e22dbb35d90fe9adeea10da96e
MatumotoTomoki <ca01254019@st.kawahara.ac.jp>
2026-07-31 10:18:13 +0900

    Item2の乱数追加

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -114,11 +114,14 @@ void Game::Update() {
 		case 1:
 		{
 			m_ui = NewGO<UI>(0, "ui");
-			m_item = NewGO<Item>(0, "item");
-			m_item2 = NewGO<Item2>(0, "item2");
-
 			int pattern = rand() % 3;
 			srand(time(nullptr));
+			m_item = NewGO<Item>(0, "item");
+			; for (int i = 0; i < 5; i++)
+			{
+				m_item2[i] = NewGO<Item2>(0, "item2");
+				m_item2[i]->SetPosition(m_item2Pos[pattern][i]);
+			}
 			for (int i = 0; i < 5; i++)
 			{
 				m_item3[i] = NewGO<Item3>(0, "item3");
@@ -242,7 +245,12 @@ void Game::Update() {
 		DeleteGO(m_distance);
 		DeleteGO(m_arrow);
 		DeleteGO(m_item);
-		DeleteGO(m_item2);
+		for (int i = 0; i < 5; i++) {
+			if (m_item2[i] != nullptr) {
+				DeleteGO(m_item2[i]);
+				m_item2[i] = nullptr;
+			}
+		}
 		DeleteGO(m_reversePengin);
 		for (int i = 0; i < 5; i++) {
 			if (m_item3[i] != nullptr) {
@@ -274,7 +282,12 @@ void Game::Update() {
 		DeleteGO(m_distance);
 		DeleteGO(m_arrow);
 		DeleteGO(m_item);
-		DeleteGO(m_item2);
+		for (int i = 0; i < 5; i++) {
+			if (m_item2[i] != nullptr) {
+				DeleteGO(m_item2[i]);
+				m_item2[i] = nullptr;
+			}
+		}
 		DeleteGO(m_reversePengin);
 		for (int i = 0; i < 5; i++) {
 			if (m_item3[i] != nullptr) {
@@ -308,7 +321,12 @@ void Game::Update() {
 		DeleteGO(m_distance);
 		DeleteGO(m_arrow);
 		DeleteGO(m_item);
-		DeleteGO(m_item2);
+		for (int i = 0; i < 5; i++) {
+			if (m_item2[i] != nullptr) {
+				DeleteGO(m_item2[i]);
+				m_item2[i] = nullptr;
+			}
+		}
 		DeleteGO(m_reversePengin);
 		for (int i = 0; i < 5; i++) {
 			if (m_item3[i] != nullptr) {
@@ -340,7 +358,12 @@ void Game::Update() {
 		DeleteGO(m_distance);
 		DeleteGO(m_arrow);
 		DeleteGO(m_item);
-		DeleteGO(m_item2);
+		for (int i = 0; i < 5; i++) {
+			if (m_item2[i] != nullptr) {
+				DeleteGO(m_item2[i]);
+				m_item2[i] = nullptr;
+			}
+		}
 		DeleteGO(m_reversePengin);
 		for (int i = 0; i < 5; i++) {
 			if (m_item3[i] != nullptr) {
--- Game/Item2.cpp
+++ Game/Item2.cpp
@@ -5,7 +5,6 @@
 
 bool Item2::Start() {
 	m_modelRender.Init("Assets/modelData/SuperMove.tkm");
-	m_position = { 0.0f,100.0f,100.0f };
 	m_modelRender.SetPosition(m_position);
 	m_player = FindGO<Player>("Player");
 	m_modelRender.Update();
@@ -25,6 +24,12 @@ void Item2::Update() {
 		se->SetVolume(finalSE);
 		DeleteGO(this);
 	}
+}
+
+void Item2::SetPosition(const Vector3& pos) {
+	m_position = pos;
+	m_modelRender.SetPosition(m_position);
+	m_modelRender.Update();
 }
 
 void Item2::Render(RenderContext& rc) {
