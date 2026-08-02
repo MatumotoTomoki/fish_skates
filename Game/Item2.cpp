2a5113147a817237414b698f65d8f5607565fea9
MitsumuneJuria <ca01254020@st.kawahara.ac.jp>
2026-08-02 10:16:44 +0900

    バフのエフェクト追従

--- Game/Item2.cpp
+++ Game/Item2.cpp
@@ -17,19 +17,32 @@ void Item2::Update() {
 	if (diff.Length() <= 80.0f) {
 		m_player->m_superMoveGet = true;
 		m_player->m_supermovetime = 0.0f;
+		//エフェクトを追従させるために削除せずに遠くまで飛ばす
+		m_position.x = 9999999999999999999.0f;
 		SoundSource* se = NewGO<SoundSource>(0);
 		se->Init(22);
 		se->Play(false);
 		float finalSE = (pause->m_sevolume / 10.0f) * (pause->m_master / 10.0f);
 		se->SetVolume(finalSE);
-		m_effectEmitter = NewGO <EffectEmitter>(0);
-		m_effectEmitter->Init(3);
-		m_effectEmitter->SetScale({ 10.0f,10.0f,10.0f });
-		m_effectEmitter->Play();
-		DeleteGO(this);
+		//エフェクトが出る回数を一回に制限
+		if (m_effectCount == 0) {
+			m_effectEmitter = NewGO <EffectEmitter>(0);
+			m_effectEmitter->Init(3);
+			m_effectEmitter->SetScale({ 10.0f,10.0f,10.0f });
+			m_effectEmitter->Play();
+			m_itemDeleteCount++;
+		}
+		m_deleteSwitch = true;
 	}
 	if (m_effectEmitter != nullptr) {
-		m_effectEmitter->SetPosition({ m_player->m_position });
+		m_effectEmitter->SetPosition(m_player->m_position);
+	}
+	//エフェクトが消えるくらいの時間でアイテムを消す
+	if (m_deleteSwitch == true) {
+		m_itemDeleteCount++;
+	}
+	if (m_itemDeleteCount >= 155) {
+		DeleteGO(this);
 	}
 }
 
