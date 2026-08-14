d0d90a958372e21da4ee41be5314d060b5e7266f
MitsumuneJuria <ca01254020@st.kawahara.ac.jp>
2026-08-14 15:05:01 +0900

    酸素ストップにも上下移動を追加

--- Game/Item.h
+++ Game/Item.h
@@ -7,9 +7,11 @@ public:
 	void Update();
 	void Render(RenderContext& rc);
 	void SetPosition(const Vector3& pos);
+	void Floating();
 	ModelRender m_modelRender;
 	PhysicsStaticObject m_physicsStaticObject;
 	Player* m_player;
 	Vector3 m_position;
-
+	float m_oldYPos;
+	float m_timer = 0.0f;
 };
