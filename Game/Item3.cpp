52766ee84ea79594d8eb965402b7def60cd4b934
MitumuneJuria <ca01254020@st.kawahara.ac.jp>
2026-08-15 14:39:38 +0900

    エフェクト関連の修正 (#255)
    * スピードアップのほうも修正を忘れていたので修正
    
    * エフェクトの制限が消えていたので再度作成

--- Game/Item3.cpp
+++ Game/Item3.cpp
@@ -32,6 +32,7 @@ void Item3::Update() {
 			m_effectEmitter->Init(4);
 			m_effectEmitter->SetScale({ 10.0f,10.0f,10.0f });
 			m_effectEmitter->Play();
+			m_effectCount++;
 		}
 		m_deleteSwitch = true;
 	}
@@ -41,7 +42,7 @@ void Item3::Update() {
 	//エフェクトが消えるくらいの時間でアイテムを消す
 	if (m_deleteSwitch == true) {
 		//エフェクトを追従させるために削除せずに見えない位置(地下深く)まで飛ばす
-		m_position.y -= -9999999999999999999.0f;
+		m_position.y = -9999999999999999999.0f;
 		m_itemDeleteCount++;
 	}
 	if (m_itemDeleteCount >= 155) {
