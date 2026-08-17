fd046602a341c48de84280c862d9f65fff1e55a8
MatumotoTomoki <ca01254019@st.kawahara.ac.jp>
2026-08-17 13:43:41 +0900

    UIの表示方法の変更
    * o2ストップもUIの表示方法の変更
    
    * スピードアップのUIの表示方法の変更
    
    * スピードダウンの表示方法の変更

--- Game/UI.cpp
+++ Game/UI.cpp
@@ -27,16 +27,32 @@ bool UI::Start() {
 	m_warningRender.SetPosition({ 0.0f,400.0f,0.0f });
 	m_o2stopRender.Init("Assets/sprite/O2stop.dds", 150.0f, 100.0f);
 	m_o2stopRender.SetPosition({ -700.0f,-100.0f,0.0f });
-	m_o2CountRender.Init("Assets/sprite/3.dds", 75.0f, 75.0f);
-	m_o2CountRender.SetPosition({ -500.0f,-100.0f,0.0f });
+	m_o2CountRender3.Init("Assets/sprite/3.dds", 75.0f, 75.0f);
+	m_o2CountRender3.SetPosition({ -500.0f,-100.0f,0.0f });
+	m_o2CountRender2.Init("Assets/sprite/2.dds", 75.0f, 75.0f);
+	m_o2CountRender2.SetPosition({ -500.0f,-100.0f,0.0f });
+	m_o2CountRender1.Init("Assets/sprite/1.dds", 75.0f, 75.0f);
+	m_o2CountRender1.SetPosition({ -500.0f,-100.0f,0.0f });
 	m_speedUpRender.Init("Assets/sprite/Speedup.dds", 150.0f, 100.0f);
 	m_speedUpRender.SetPosition({ -700.0f,-50.0f,0.0f });
-	m_speedCountRender.Init("Assets/sprite/3.dds", 75.0f, 75.0f);
-	m_speedCountRender.SetPosition({ -500.0f,-50.0f,0.0f });
+	m_speedCountRender3.Init("Assets/sprite/3.dds", 75.0f, 75.0f);
+	m_speedCountRender3.SetPosition({ -500.0f,-50.0f,0.0f });
+	m_speedCountRender2.Init("Assets/sprite/2.dds", 75.0f, 75.0f);
+	m_speedCountRender2.SetPosition({ -500.0f,-50.0f,0.0f });
+	m_speedCountRender1.Init("Assets/sprite/1.dds", 75.0f, 75.0f);
+	m_speedCountRender1.SetPosition({ -500.0f,-50.0f,0.0f });
 	m_speedDownRender.Init("Assets/sprite/Speeddowncount.dds", 150.0f, 100.0f);
 	m_speedDownRender.SetPosition({ -700.0f,0.0f,0.0f });
-	m_downCountRender.Init("Assets/sprite/5.dds", 75.0f, 75.0f);
-	m_downCountRender.SetPosition({ -500.0f,0.0f,0.0f });
+	m_downCountRender5.Init("Assets/sprite/5.dds", 75.0f, 75.0f);
+	m_downCountRender5.SetPosition({ -500.0f,0.0f,0.0f });
+	m_downCountRender4.Init("Assets/sprite/4.dds", 75.0f, 75.0f);
+	m_downCountRender4.SetPosition({ -500.0f,0.0f,0.0f });
+	m_downCountRender3.Init("Assets/sprite/3.dds", 75.0f, 75.0f);
+	m_downCountRender3.SetPosition({ -500.0f,0.0f,0.0f });
+	m_downCountRender2.Init("Assets/sprite/2.dds", 75.0f, 75.0f);
+	m_downCountRender2.SetPosition({ -500.0f,0.0f,0.0f });
+	m_downCountRender1.Init("Assets/sprite/1.dds", 75.0f, 75.0f);
+	m_downCountRender1.SetPosition({ -500.0f,0.0f,0.0f });
 	return true;
 }
 
@@ -169,47 +185,45 @@ void UI::Update() {
 	if (m_player->m_o2StopGet == true) {
 		m_o2stopRender.Update();
 		if (m_player->m_o2stoptime >= 0.0f and m_player->m_o2stoptime < 1.0f) {
-			m_o2CountRender.Init("Assets/sprite/3.dds", 75.0f, 75.0f);
+			m_o2CountRender3.Update();
 		}
 		if (m_player->m_o2stoptime >= 1.0f and m_player->m_o2stoptime < 2.0f) {
-			m_o2CountRender.Init("Assets/sprite/2.dds", 75.0f, 75.0f);
+			m_o2CountRender2.Update();
 		}
 		if (m_player->m_o2stoptime >= 2.0f) {
-			m_o2CountRender.Init("Assets/sprite/1.dds", 75.0f, 75.0f);
+			m_o2CountRender1.Update();
 		}
-		m_o2CountRender.Update();
+		
 	}
 	if (m_player->m_superMoveGet == true) {
 		m_speedUpRender.Update();
 		if (m_player->m_supermovetime >= 0.0f and m_player->m_supermovetime < 1.0f) {
-			m_speedCountRender.Init("Assets/sprite/3.dds", 75.0f, 75.0f);
+			m_speedCountRender3.Update();
 		}
 		else if (m_player->m_supermovetime >= 1.0f and m_player->m_supermovetime < 2.0f) {
-			m_speedCountRender.Init("Assets/sprite/2.dds", 75.0f, 75.0f);
+			m_speedCountRender2.Update();
 		}
 		else {
-			m_speedCountRender.Init("Assets/sprite/1.dds", 75.0f, 75.0f);
+			m_speedCountRender1.Update();
 		}
-		m_speedCountRender.Update();
 	}
 	if (m_player->m_getaCount > 0) {
 		m_speedDownRender.Update();
 		if (m_player->m_getaCount == 1) {
-			m_downCountRender.Init("Assets/sprite/1.dds", 75.0f, 75.0f);
+			m_downCountRender1.Update();
 		}
 		else if (m_player->m_getaCount == 2) {
-			m_downCountRender.Init("Assets/sprite/2.dds", 75.0f, 75.0f);
+			m_downCountRender2.Update();
 		}
 		else if (m_player->m_getaCount == 3) {
-			m_downCountRender.Init("Assets/sprite/3.dds", 75.0f, 75.0f);
+			m_downCountRender3.Update();
 		}
 		else if (m_player->m_getaCount == 4) {
-			m_downCountRender.Init("Assets/sprite/4.dds", 75.0f, 75.0f);
+			m_downCountRender4.Update();
 		}
 		else {
-			m_downCountRender.Init("Assets/sprite/5.dds", 75.0f, 75.0f);
+			m_downCountRender5.Update();
 		}
-		m_downCountRender.Update();
 	}
 }
 
@@ -231,43 +245,43 @@ void UI::Render(RenderContext& rc) {
 		if (m_player->m_getaCount > 0) {
 			m_speedDownRender.Draw(rc);
 			if (m_player->m_getaCount == 1) {
-				m_downCountRender.Draw(rc);
+				m_downCountRender1.Draw(rc);
 			}
 			else if (m_player->m_getaCount == 2) {
-				m_downCountRender.Draw(rc);
+				m_downCountRender2.Draw(rc);
 			}
 			else if (m_player->m_getaCount == 3) {
-				m_downCountRender.Draw(rc);
+				m_downCountRender3.Draw(rc);
 			}
 			else if (m_player->m_getaCount == 4) {
-				m_downCountRender.Draw(rc);
+				m_downCountRender4.Draw(rc);
 			}
 			else {
-				m_downCountRender.Draw(rc);
+				m_downCountRender5.Draw(rc);
 			}
 		}
 		if (m_player->m_superMoveGet == true) {
 			m_speedUpRender.Draw(rc);
 			if (m_player->m_supermovetime >= 0.0f and m_player->m_supermovetime < 1.0f) {
-				m_o2CountRender.Draw(rc);
+				m_speedCountRender3.Draw(rc);
 			}
 			else if (m_player->m_supermovetime >= 1.0f and m_player->m_supermovetime < 2.0f) {
-				m_o2CountRender.Draw(rc);
+				m_speedCountRender2.Draw(rc);
 			}
 			else {
-				m_o2CountRender.Draw(rc);
+				m_speedCountRender1.Draw(rc);
 			}
 		}
 		if (m_player->m_o2StopGet == true) {
 			m_o2stopRender.Draw(rc);
 			if (m_player->m_o2stoptime >= 0.0f and m_player->m_o2stoptime < 1.0f) {
-				m_speedCountRender.Draw(rc);
+				m_o2CountRender3.Draw(rc);
 			}
 			if (m_player->m_o2stoptime >= 1.0f and m_player->m_o2stoptime < 2.0f) {
-				m_speedCountRender.Draw(rc);
+				m_o2CountRender2.Draw(rc);
 			}
 			if (m_player->m_o2stoptime >= 2.0f) {
-				m_speedCountRender.Draw(rc);
+				m_o2CountRender1.Draw(rc);
 			}
 		}
 	}
