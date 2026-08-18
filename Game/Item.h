d897be65df37727534c3efcb6aafc525f9a64463
MatumotoTomoki <ca01254019@st.kawahara.ac.jp>
2026-08-18 10:21:06 +0900

    酸素ストップにエフェクトを追加

--- Game/Item.h
+++ Game/Item.h
@@ -12,6 +12,10 @@ public:
 	PhysicsStaticObject m_physicsStaticObject;
 	Player* m_player;
 	Vector3 m_position;
+	EffectEmitter* m_effectEmitter = nullptr;
 	float m_oldYPos;
 	float m_timer = 0.0f;
+	int m_effectCount = 0;
+	bool m_deleteSwitch = false;
+	int m_itemDeleteCount = 0;
 };
